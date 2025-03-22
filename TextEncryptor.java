1fe9d9e1b6d7d0f6fdda0c37d5b9b8032838352c
Samarth Narula <v_snarula@adt.com>
2025-03-22 15:45:52 -0400

    Model dyanmic attempt 6

--- TextEncryptor.java
+++ TextEncryptor.java
@@ -35,7 +35,7 @@ public class TextEncryptor {
 
     public static void main(String[] args) {
         try {
-            String plainText = "This is a secret message. 1234";
+            String plainText = "This is a secret message. 12345";
             String encryptedText = encrypt(plainText);
             System.out.println("Encrypted text: " + encryptedText);
         } catch (Exception e) {
